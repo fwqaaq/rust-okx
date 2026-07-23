1c40381c0d4c530f8ae17ade1e90d74195a0ed32
fwqaaq <fwqaaq@gmail.com>
2026-07-23 21:46:32 +0800

    feat(trading-bot): implement grid endpoints

---
+++ src/api/trading_bot/endpoints.rs
@@ -0,0 +1,25 @@
+pub(super) const GRID_ORDER: &str = "/api/v5/tradingBot/grid/order-algo";
+pub(super) const GRID_AMEND_BASIC: &str = "/api/v5/tradingBot/grid/amend-algo-basic-param";
+pub(super) const GRID_AMEND: &str = "/api/v5/tradingBot/grid/amend-order-algo";
+pub(super) const GRID_STOP: &str = "/api/v5/tradingBot/grid/stop-order-algo";
+pub(super) const GRID_CLOSE_POSITION: &str = "/api/v5/tradingBot/grid/close-position";
+pub(super) const GRID_CANCEL_CLOSE: &str = "/api/v5/tradingBot/grid/cancel-close-order";
+pub(super) const GRID_INSTANT_TRIGGER: &str =
+    "/api/v5/tradingBot/grid/order-instant-trigger";
+pub(super) const GRID_PENDING: &str = "/api/v5/tradingBot/grid/orders-algo-pending";
+pub(super) const GRID_HISTORY: &str = "/api/v5/tradingBot/grid/orders-algo-history";
+pub(super) const GRID_DETAILS: &str = "/api/v5/tradingBot/grid/orders-algo-details";
+pub(super) const GRID_SUB_ORDERS: &str = "/api/v5/tradingBot/grid/sub-orders";
+pub(super) const GRID_POSITIONS: &str = "/api/v5/tradingBot/grid/positions";
+pub(super) const GRID_WITHDRAW_INCOME: &str = "/api/v5/tradingBot/grid/withdraw-income";
+pub(super) const GRID_COMPUTE_MARGIN: &str =
+    "/api/v5/tradingBot/grid/compute-margin-balance";
+pub(super) const GRID_MARGIN_BALANCE: &str = "/api/v5/tradingBot/grid/margin-balance";
+pub(super) const GRID_ADJUST_INVESTMENT: &str =
+    "/api/v5/tradingBot/grid/adjust-investment";
+pub(super) const GRID_AI_PARAM: &str = "/api/v5/tradingBot/grid/ai-param";
+pub(super) const GRID_MIN_INVESTMENT: &str = "/api/v5/tradingBot/grid/min-investment";
+pub(super) const GRID_RSI_BACK_TESTING: &str =
+    "/api/v5/tradingBot/public/rsi-back-testing";
+pub(super) const GRID_QUANTITY: &str = "/api/v5/tradingBot/grid/grid-quantity";
+pub(super) const GRID_COPY_ORDER: &str = "/api/v5/tradingBot/grid/copy-order-algo";
