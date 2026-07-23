cea0d7f62679899182373c995c464e11c30f43ca
fwqaaq <fwqaaq@gmail.com>
2026-07-23 21:14:41 +0800

    feat(finance): implement OKUSD endpoints

--- src/api/finance/responses.rs
+++ src/api/finance/responses.rs
@@ -1,11 +1,13 @@
 mod dual_investment;
 mod flexible_loan;
+mod okusd;
 mod savings;
 mod stable_rewards;
 mod staking;
 
 pub use dual_investment::*;
 pub use flexible_loan::*;
+pub use okusd::*;
 pub use savings::*;
 pub use stable_rewards::*;
 pub use staking::*;
