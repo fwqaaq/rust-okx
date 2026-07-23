92fb0b8b841f0cedc7156dcde5371ff0a3954e6a
fwqaaq <fwqaaq@gmail.com>
2026-07-23 20:56:37 +0800

    feat(trading-data): complete REST endpoints (#32)
    * feat(trading-data): complete REST endpoints
    
    * feat(trading-data): complete REST endpoints
    
    * feat(trading-data): complete REST endpoints
    
    * feat(trading-data): complete REST endpoints
    
    * feat(trading-data): complete REST endpoints
    
    * feat(trading-data): complete REST endpoints
    
    * feat(trading-data): complete REST endpoints
    
    * feat(trading-data): complete REST endpoints
    
    * feat(trading-data): complete REST endpoints
    
    * feat(trading-data): complete REST endpoints
    
    * feat(trading-data): complete REST endpoints
    
    * style(trading-data): apply rustfmt
    
    * style(trading-data): apply rustfmt
    
    * style(trading-data): apply rustfmt
    
    * docs(trading-data): document response fields

---
+++ src/api/trading_data/endpoints.rs
@@ -0,0 +1,25 @@
+pub(super) const SUPPORT_COINS: &str = "/api/v5/rubik/stat/trading-data/support-coin";
+pub(super) const CONTRACT_OPEN_INTEREST_HISTORY: &str =
+    "/api/v5/rubik/stat/contracts/open-interest-history";
+pub(super) const TAKER_VOLUME: &str = "/api/v5/rubik/stat/taker-volume";
+pub(super) const CONTRACT_TAKER_VOLUME: &str = "/api/v5/rubik/stat/taker-volume-contract";
+pub(super) const MARGIN_LOAN_RATIO: &str = "/api/v5/rubik/stat/margin/loan-ratio";
+pub(super) const TOP_TRADER_ACCOUNT_RATIO: &str =
+    "/api/v5/rubik/stat/contracts/long-short-account-ratio-contract-top-trader";
+pub(super) const TOP_TRADER_POSITION_RATIO: &str =
+    "/api/v5/rubik/stat/contracts/long-short-position-ratio-contract-top-trader";
+pub(super) const CONTRACT_LONG_SHORT_ACCOUNT_RATIO: &str =
+    "/api/v5/rubik/stat/contracts/long-short-account-ratio-contract";
+pub(super) const LONG_SHORT_ACCOUNT_RATIO: &str =
+    "/api/v5/rubik/stat/contracts/long-short-account-ratio";
+pub(super) const CONTRACT_OPEN_INTEREST_VOLUME: &str =
+    "/api/v5/rubik/stat/contracts/open-interest-volume";
+pub(super) const OPTION_OPEN_INTEREST_VOLUME: &str =
+    "/api/v5/rubik/stat/option/open-interest-volume";
+pub(super) const OPTION_PUT_CALL_RATIO: &str =
+    "/api/v5/rubik/stat/option/open-interest-volume-ratio";
+pub(super) const OPTION_OPEN_INTEREST_VOLUME_EXPIRY: &str =
+    "/api/v5/rubik/stat/option/open-interest-volume-expiry";
+pub(super) const OPTION_OPEN_INTEREST_VOLUME_STRIKE: &str =
+    "/api/v5/rubik/stat/option/open-interest-volume-strike";
+pub(super) const OPTION_TAKER_FLOW: &str = "/api/v5/rubik/stat/option/taker-block-volume";
