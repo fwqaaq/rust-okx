48c89f9d65646660dd652f8e9c1a26e1983889a7
fwqaaq <fwqaaq@gmail.com>
2026-08-10 05:16:03 +0800

    feat: implement Copy Trading REST API

---
+++ src/api/copy_trading/endpoints.rs
@@ -0,0 +1,32 @@
+pub(super) const CURRENT_SUBPOSITIONS: &str = "/api/v5/copytrading/current-subpositions";
+pub(super) const SUBPOSITIONS_HISTORY: &str = "/api/v5/copytrading/subpositions-history";
+pub(super) const ALGO_ORDER: &str = "/api/v5/copytrading/algo-order";
+pub(super) const CLOSE_SUBPOSITION: &str = "/api/v5/copytrading/close-subposition";
+pub(super) const INSTRUMENTS: &str = "/api/v5/copytrading/instruments";
+pub(super) const SET_INSTRUMENTS: &str = "/api/v5/copytrading/set-instruments";
+pub(super) const PROFIT_SHARING_DETAILS: &str = "/api/v5/copytrading/profit-sharing-details";
+pub(super) const TOTAL_PROFIT_SHARING: &str = "/api/v5/copytrading/total-profit-sharing";
+pub(super) const UNREALIZED_PROFIT_SHARING_DETAILS: &str =
+    "/api/v5/copytrading/unrealized-profit-sharing-details";
+pub(super) const TOTAL_UNREALIZED_PROFIT_SHARING: &str =
+    "/api/v5/copytrading/total-unrealized-profit-sharing";
+pub(super) const AMEND_PROFIT_SHARING_RATIO: &str =
+    "/api/v5/copytrading/amend-profit-sharing-ratio";
+pub(super) const CONFIG: &str = "/api/v5/copytrading/config";
+pub(super) const FIRST_COPY_SETTINGS: &str = "/api/v5/copytrading/first-copy-settings";
+pub(super) const AMEND_COPY_SETTINGS: &str = "/api/v5/copytrading/amend-copy-settings";
+pub(super) const STOP_COPY_TRADING: &str = "/api/v5/copytrading/stop-copy-trading";
+pub(super) const COPY_SETTINGS: &str = "/api/v5/copytrading/copy-settings";
+pub(super) const CURRENT_LEAD_TRADERS: &str = "/api/v5/copytrading/current-lead-traders";
+pub(super) const PUBLIC_CONFIG: &str = "/api/v5/copytrading/public-config";
+pub(super) const PUBLIC_LEAD_TRADERS: &str = "/api/v5/copytrading/public-lead-traders";
+pub(super) const PUBLIC_WEEKLY_PNL: &str = "/api/v5/copytrading/public-weekly-pnl";
+pub(super) const PUBLIC_PNL: &str = "/api/v5/copytrading/public-pnl";
+pub(super) const PUBLIC_STATS: &str = "/api/v5/copytrading/public-stats";
+pub(super) const PUBLIC_PREFERENCE_CURRENCY: &str =
+    "/api/v5/copytrading/public-preference-currency";
+pub(super) const PUBLIC_CURRENT_SUBPOSITIONS: &str =
+    "/api/v5/copytrading/public-current-subpositions";
+pub(super) const PUBLIC_SUBPOSITIONS_HISTORY: &str =
+    "/api/v5/copytrading/public-subpositions-history";
+pub(super) const PUBLIC_COPY_TRADERS: &str = "/api/v5/copytrading/public-copy-traders";
